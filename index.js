b24863eeef45fbaef4611ae8ea687c0c570e7abe
NoaDa <noadahan3@gmail.com>
2023-06-17 17:56:44 +0300

    adding tabels, crud

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const stringify = require('csv-stringify').stringify;
 const { parse } = require("csv-parse");
 const CSVToJSON = require('csvtojson');
 const bodyParser = require("body-parser");
+const multer = require('multer'); 
+const upload = multer(); 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true
 }));
@@ -17,6 +20,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.get('/', (req, res) => {
   res.render('homepage.html');
@@ -27,8 +35,18 @@ app.get('/register', (req, res) => {
 });
 
 app.get('/CreateTable_DoggySitters',CreateDB.CreateTable_DoggySitters);
-app.post('/Create_DoggySitter',CRUD.createNewDoggySitter);
+app.post('/Create_DoggySitter', upload.none(), CRUD.createNewDoggySitter);
+
+app.get('/CreateTable_Dogs',CreateDB.CreateTable_Dogs);
+app.post('/Create_Dog', upload.none(), CRUD.createNewDog);
+
+app.get('/CreateTable_DogOwners',CreateDB.CreateTable_DogOwners);
+app.post('/Create_DogOwner', upload.none(), CRUD.createNewDogOwner)
+
+app.get('/CreateTable_SittingHistory',CreateDB.CreateTable_SittingHistory);
+
+
+
 app.listen(port,()=>{
     console.log("Server is running on port"+port)
-    });
-
+  });
